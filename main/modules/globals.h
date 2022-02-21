c59500cf629ea409783d4d997f703d248389a272
BM45 <48355256+BM45@users.noreply.github.com>
2022-02-21 10:45:27 +0100

    Update globals.h

--- main/modules/globals.h
+++ main/modules/globals.h
@@ -6,6 +6,11 @@
 
 //#define USE_INTERNAL_AUDIODAC
 
+// override by menuconfig
+#ifdef CONFIG_USE_INTERNAL_AUDIODAC
+  #define USE_INTERNAL_AUDIODAC
+#endif
+
 #define MAX_CHANNELS_IN_LIST 100
 char* playlist[MAX_CHANNELS_IN_LIST];
 int   channels_in_list;
