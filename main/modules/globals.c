30f58a753b21f45f9d16109367dba947be2c43b6
BM45 <48355256+BM45@users.noreply.github.com>
2023-02-05 10:26:20 +0100

    Add files via upload

---
+++ main/modules/globals.c
@@ -0,0 +1,17 @@
+#include "globals.h"
+
+char* playlist[MAX_CHANNELS_IN_LIST];
+int   channels_in_list;
+
+// set by wifi.txt and playlist.m3u from SDCARD
+periph_wifi_cfg_t wifi_cfg;
+esp_periph_set_handle_t set;
+
+// set by AM.txt form SDCARD
+bool TX_ENABLED;
+uint32_t am_tx_freq;
+
+// mediaplayerd globals
+bool MEDIAPLAYER_ENABLED; 
+playlist_operator_handle_t sdcard_list_handle;
+
